cebb17a800082aaaa65f657cb1dc17427690a8ac
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-07-22 13:55:23 -0500

    lad88- feat: data binding de la leccion en HTML usando interpolacion segura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeccionService } from './services/leccion.service';
+import { LeccionCompletaDTO } from './models/leccion.dto'; //1. importas la forma de la caja
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,8 +10,21 @@ import { LeccionService } from './services/leccion.service';
 })
 export class App {
   protected readonly title = signal('frontend-tesis');
+  //2. Creas la "mesa vacia". Al principio, no hay leccion(undefined).
+  leccionActual: LeccionCompletaDTO | undefined;
 
   constructor(private leccionService: LeccionService) {
-    this.leccionService.obtenerLeccion(1).subscribe();
+    //3. Envias el mensajero y firmas la orden()
+    this.leccionService.obtenerLeccion(1).subscribe({
+      next: (datosQueLlegaron) => {
+        //4. Magicamente,2 segundos depues,el mensajero vuelve.
+        //Pones la comida en la mesa:
+        this.leccionActual = datosQueLlegaron;
+        console.log('!Los datos reales llegaron', this.leccionActual);
+      },
+      error: (error) => {
+        console.error('El mensajero fracasó', error);
+      },
+    });
   }
 }
