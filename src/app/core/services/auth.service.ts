a4d5ba8839a149e70e00a3dcda154815f78cd555
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-07-29 05:41:25 -0500

    lad93 (parte3) feat: manejo de estado global con Signals para datos del usuario

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -17,6 +17,21 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   iniciarSesion(credenciales: AuthRequestDTO): Observable<AuthResponseDTO> {
-    return this.http.post<AuthResponseDTO>(`${this.apiUrl}/login`, credenciales).pipe();
+    return this.http.post<AuthResponseDTO>(`${this.apiUrl}/login`, credenciales).pipe(
+      // 4. EL GUARDIA GUARDA EL PASE VIP EN TU BOLSILLO
+      tap((respuesta) => {
+        // 1. Guardamos la llave en el bolsillo del navegador
+        localStorage.setItem('token', respuesta.token);
+        // 2. Escribimos en la Pizarra Central
+        this.tokenActual.set(respuesta.token);
+      }),
+    );
   }
+
+  cerrarSesion() {
+    //1. Borramos la llave del bolsillo
+    localStorage.removeItem('token');
+    //2. Borramos la Pizarra (tocamos la campana avisando que ya no hay usuario)
+    this.tokenActual.set(null);
+  } // Fin de la clase
 }
