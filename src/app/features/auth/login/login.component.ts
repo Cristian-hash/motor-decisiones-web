8884f8acbbd757f00907e981e1f1061f530b5680
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-07-31 04:57:35 -0500

    lad94-feat: orquestacion de flujo asincrono de login y redireccion

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //<-- Herrameinta que lee las cajas de texto
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { AuthRequestDTO } from '../../../models/auth.dto';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // 1. Mis expertos contratados
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  //2. El pergamino en blanco con la forma exacta del DTO
+  credenciales: AuthRequestDTO = {
+    email: '',
+    password: '',
+  };
+  //3. El cartel de error (inicia limpio)
+  mensajeError: string = '';
+}
