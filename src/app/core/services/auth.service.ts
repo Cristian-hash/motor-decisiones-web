9b064f26979d8b33b3e9e643b9f9d308d0581151
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-08-06 09:21:37 -0500

    lad96 feat: implementacion de loout ,limpieza de estado y redireccion

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,20 +1,21 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { AuthRequestDTO, AuthResponseDTO } from '../../models/auth.dto';
-
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   //La direccion de tu Spring Boot
   private apiUrl = 'http://localhost:8080/api/v1/auth';
-
+  private http = inject(HttpClient);
+  private router = inject(Router);
   //NUESTRA PIZARRA CENTRAL (signal)
   //Guardaremos el token aqui para que toda la app sepa si hay alguien conectado.
   // Inicia leyendo el localStorage por si el usuario recargo la pagina(F5)
   tokenActual = signal<string | null>(localStorage.getItem('token'));
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   iniciarSesion(credenciales: AuthRequestDTO): Observable<AuthResponseDTO> {
     return this.http.post<AuthResponseDTO>(`${this.apiUrl}/login`, credenciales).pipe(
@@ -27,11 +28,12 @@ export class AuthService {
       }),
     );
   }
-
   cerrarSesion() {
+    console.log('Destruyendo llaves y cerrando sesión');
     //1. Borramos la llave del bolsillo
     localStorage.removeItem('token');
-    //2. Borramos la Pizarra (tocamos la campana avisando que ya no hay usuario)
     this.tokenActual.set(null);
+    //2. Borramos la Pizarra (tocamos la campana avisando que ya no hay usuario)
+    this.router.navigate(['/login']);
   } // Fin de la clase
 }
